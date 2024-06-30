0b25e68c166479d177f8ca69ef2b19dfae7e6ef3
araiyutaro <meganecatcher27@gmail.com>
2024-06-30 23:03:54 +0900

    sele.pyに繰り返し処理を追加、README.mdを追加

--- sele.py
+++ sele.py
@@ -2,10 +2,9 @@ import urllib.parse
 from urllib.parse import urlparse
 
 import spacy
-from publicsuffix2 import  get_sld
+from publicsuffix2 import get_sld
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 
 url_suntory = "https://www.suntory.co.jp/"
 url_kirin = "https://www.kirin.co.jp/"
@@ -33,16 +32,14 @@ def get_same_domain_links(url=""):
     for element in a_elements:
         u = urllib.parse.urlparse(element.get_attribute('href'))
         links.append(u.scheme + "://" + u.netloc + u.path)
-    print(links)
-    print(len(links))
     links = set(links)
-    print(links)
-    print(len(links))
     links = list(filter(lambda x: is_same_domain(domain, x), links))
     links = list(filter(lambda x: x != url, links))
-    print(links)
-    print(len(links))
+    links = set(links)
     driver.quit()
+    print("same_domain_links:")
+    print(links)
+    print("リンク先の数：" + str(len(links)))
     return links
 
 
@@ -68,18 +65,43 @@ def get_website_word_list(url=""):
         for token in doc:
             if token.tag_ in ["名詞-普通名詞-一般"]:
                 words_list.append(token.lemma_)
+    driver.quit()
+    print("website_word_list:")
+    print(url)
+    print(words_list)
+    print("単語数：" + str(len(words_list)))
     return words_list
 
 
-links_suntory = get_same_domain_links(url_suntory)
-links_kirin = get_same_domain_links(url_kirin)
-print(links_suntory)
-print(len(links_suntory))
-print(links_kirin)
-print(len(links_kirin))
-words_suntory = get_website_word_list(url_suntory)
-words_kirin = get_website_word_list(url_kirin)
-print(words_suntory)
-print(len(words_suntory))
-print(words_kirin)
-print(len(words_kirin))
+def get_all_words_list(url=""):
+    links = get_same_domain_links(url)
+    words = get_website_word_list(url)
+    for link in links:
+        words = words + get_website_word_list(link)
+    return words
+
+
+# links_suntory = get_same_domain_links(url_suntory)
+# links_kirin = get_same_domain_links(url_kirin)
+# print(links_suntory)
+# print(len(links_suntory))
+# print(links_kirin)
+# print(len(links_kirin))
+# words_suntory = get_website_word_list(url_suntory)
+# words_kirin = get_website_word_list(url_kirin)
+# print(words_suntory)
+# print(len(words_suntory))
+# print(words_kirin)
+# print(len(words_kirin))
+all_words_suntory = get_all_words_list(url_suntory)
+print("all_words:")
+print(all_words_suntory)
+print("全単語数" + str(len(all_words_suntory)))
+all_words_set = set(all_words_suntory)
+result = []
+for word in all_words_set:
+    word_count = all_words_suntory.count(word)
+    result.append((word, word_count))
+result.sort(key=lambda x: x[1], reverse=True)
+print("〜〜〜全単語とその頻度〜〜〜")
+print(result)
